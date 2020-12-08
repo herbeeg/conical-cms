6a30f5372556aafd2ae0078615aebeb98c15000e
Jon Herbst <herbtaculargames@gmail.com>
2020-12-08 11:16:42 +0000

    Adding first test file for root requests to start building passing criteria

---
+++ tests/test_app.py
@@ -0,0 +1,37 @@
+import pytest
+
+from pathlib import Path
+
+TEST_DB = 'test.db'
+
+class TestMainCase:
+    @pytest.fixture
+    def client(self):
+        BASE_DIR = Path(__file__).resolve().parent.parent
+
+        self.app = create_app()
+        self.app.app_context().push()
+
+        self.app.config['TESTING'] = True
+        self.app.config['DATABASE'] = BASE_DIR.joinpath(TEST_DB)
+        self.app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{BASE_DIR.joinpath(TEST_DB)}'
+
+        self.app.config['EMAIL'] = 'admin@test.com'
+        self.app.config['USERNAME'] = 'admin'
+        self.app.config['PASSWORD'] = 'password'
+
+        db.create_all()
+
+        with self.app.test_client(self) as client:
+            yield client
+        
+        db.drop_all()
+
+    def testIndex(self, client):
+        response = client.get(
+            '/',
+            content_type='html/text'
+        )
+
+        assert 200 == response.status_code
+        assert b'There is no ignorance, there is knowledge.' == response.data        
